1245967a3c7aac5761fe0d1b9bf23400dc582e9e
jeremiah <jeremiahbl@protonmail.com>
2026-05-08 02:01:57 -0500

    ddmafb.c: fixups and make it work better

--- ddmafb.c
+++ ddmafb.c
@@ -75,93 +75,101 @@ static int ddmafb_setcolreg(u_int regno, u_int red, u_int green, u_int blue,
 
 struct ddmafb_par {
 	u32 palette[PSEUDO_PALETTE_SIZE];
+	u32 frametime_us;
 	struct device *dev;
 	struct task_struct *dma_thread;
 	struct dma_chan *dma_channel;
 	dma_addr_t dma_handle;
 	void *dma_cpu_addr;
 	size_t dma_size;
+	bool complete;
 };
+
+static void ddmafb_dma_callback(void *data) {
+	struct ddmafb_par *par = data;
+	par->complete = true;
+}
+
+static int ddmafb_thread_init(struct ddmafb_par *par) {
+	struct dma_slave_config cfg;
+	int ret = 0;
+
+	memset(&cfg, 0, sizeof(struct dma_slave_config));
+
+	/* Configure the channel */
+	cfg.direction       = DMA_MEM_TO_DEV;
+	cfg.dst_addr        = 0; /* AXI DMA uses stream interface; often 0 */
+	cfg.dst_addr_width  = DMA_SLAVE_BUSWIDTH_4_BYTES;
+	cfg.dst_maxburst    = 16;
+	ret = dmaengine_slave_config(par->dma_channel, &cfg);
+	if (ret) {
+		dev_err(par->dev, "Failed to configure DMA channel: %d\n", ret);
+		return ret;
+	}
+	//dev_info(par->dev, "Configured 4-byte wide DMA_MEM_TO_DEV slave interface!\n");
+	return 0;
+}
+
+static int ddmafb_do_xfer(struct ddmafb_par *par) {
+	struct dma_async_tx_descriptor *desc;
+	dma_cookie_t cookie;
+
+	/* Prepare the descriptor */
+	desc = dmaengine_prep_slave_single(
+		par->dma_channel,
+		par->dma_handle,            /* DMA address of buffer */
+		par->dma_size,              /* length in bytes */
+		DMA_MEM_TO_DEV,             /* direction */
+		DMA_PREP_INTERRUPT |        /* fire callback on completion */
+		DMA_CTRL_ACK                /* auto-ack the descriptor */
+	);
+	if (!desc) {
+		dev_err(par->dev, "Failed to prepare DMA descriptor\n");
+		return -ENOMEM;
+	}
+	desc->callback       = ddmafb_dma_callback;
+	desc->callback_param = par;
+	//dev_info("DMA descriptor created at 0x%08X, size 0x%08X!\n", par->dev, par->dma_handle, par->dma_size);
+
+	/* Submit the descriptor */
+	cookie = dmaengine_submit(desc);
+	if (dma_submit_error(cookie)) {
+		dev_err(par->dev, "DMA submit failed\n");
+		return -EIO;
+	}
+	dma_async_issue_pending(par->dma_channel);
+	//dev_info(par->dev, "DMA Descriptor submitted to DMA engine\n");
+	return 0;
+}
 
 static int ddmafb_thread_fn(void *data) {
 	struct ddmafb_par *par = data;
-	struct dma_async_tx_descriptor *desc;
-	struct dma_slave_config cfg;
-	struct dma_chan *chan;
-	dma_addr_t dma_handle;
-	dma_cookie_t cookie;
-	enum dma_status status;
-	size_t dma_size;
-	int ret, submit;
+	ktime_t last, cur;
+	int ret;
 
 	if(!par) {
 		pr_err("ddmafb_thread_fn: ddmafb_par pointer is NULL\n");
 		return -ENODEV;
 	}
-	if(IS_ERR(par)) {
-		pr_err("ddmafb_thread_fn: ddmafb_par pointer is error: \n", PTR_ERR(par));
-		return PTR_ERR(par);
-	}
 
-	chan = par->dma_channel;
-	dma_size = par->dma_size;
-	dma_handle = par->dma_handle;
-
-	/* Configure the channel */
-	pr_info("dmaengine_slave_config\n");
-	memset(&cfg, 0, sizeof(struct dma_slave_config));
-	cfg.direction       = DMA_MEM_TO_DEV;
-	cfg.dst_addr        = 0; /* AXI DMA uses stream interface; often 0 */
-	cfg.dst_addr_width  = DMA_SLAVE_BUSWIDTH_4_BYTES;
-	cfg.dst_maxburst    = 16;
-	ret = dmaengine_slave_config(chan, &cfg);
-	if (ret) {
-		dev_err(par->dev, "Failed to configure DMA channel: %d\n", ret);
+	ret = ddmafb_thread_init(par);
+	if(ret)
 		return ret;
-	}
 
 	/* Start doing transfers */
-	submit = 1;
+	par->complete = true;
 	while (!kthread_should_stop()) {
-		if (submit) {
-			/* Prepare the descriptor */
-			pr_info("dmaengine_prep_slave_single\n");
-			desc = dmaengine_prep_slave_single(
-				chan,
-				dma_handle,                 /* DMA address of buffer */
-				dma_size,                   /* length in bytes */
-				DMA_MEM_TO_DEV,             /* direction */
-				0
-				//DMA_PREP_INTERRUPT |        /* fire callback on completion */
-				//DMA_CTRL_ACK                /* auto-ack the descriptor */
-			);
-			if (!desc) {
-				dev_err(par->dev, "Failed to prepare DMA descriptor\n");
-				return -ENOMEM;
+		cur = ktime_get();
+		if (par->frametime_us == 0 || ktime_to_us(ktime_sub(cur, last)) >= par->frametime_us) {
+			if (par->complete) {
+				last = cur;
+				par->complete = false;
+				ret = ddmafb_do_xfer(par);
+				//dev_info(par->dev, "Transfer done, starting another\n");
+				continue;
 			}
-			pr_info("dma descriptor @ 0x%08X + 0x%08X\n", dma_handle, dma_size);
-
-			dev_info(par->dev, "dmaengine_submit\n");
-			cookie = dmaengine_submit(desc);
-			if (dma_submit_error(cookie)) {
-				dev_err(par->dev, "DMA submit failed\n");
-				return -EIO;
-			}
-
-			dev_info(par->dev, "dma_async_issue_pending\n");
-			dma_async_issue_pending(chan);
-			submit = 0;
 		}
-
-		dev_info(par->dev, "dma_async_is_tx_complete\n");
-		status = dma_async_is_tx_complete(chan, cookie, NULL, NULL);
-		if (status == DMA_COMPLETE) {
-			dev_info(par->dev, "Transfer done, starting another\n");
-			submit = 1;
-			ssleep(1);  // sleep 1 second
-		} else {
-			msleep(100);
-		}
+		msleep(1);
 	}
 	return 0;
 }
@@ -175,8 +183,8 @@ static void ddmafb_destroy(struct fb_info *info) {
 
 	kthread_stop(par->dma_thread);
 
-	if (info->screen_base)
-		iounmap(info->screen_base);
+	//if (info->screen_base)
+	//	iounmap(info->screen_base);
 
 	framebuffer_release(info);
 }
@@ -226,12 +234,6 @@ static int ddmafb_parse_dt(struct platform_device *pdev,
 		return ret;
 	}
 
-	ret = of_property_read_u32(np, "framerate", &params->framerate);
-	if (ret) {
-		dev_err(&pdev->dev, "Can't parse framerate property\n");
-		return ret;
-	}
-
 	ret = of_property_read_string(np, "format", &str);
 	if (ret) {
 		dev_err(&pdev->dev, "Can't parse format property\n");
@@ -247,6 +249,8 @@ static int ddmafb_parse_dt(struct platform_device *pdev,
 		dev_err(&pdev->dev, "Invalid format value\n");
 		return -EINVAL;
 	}
+
+	of_property_read_u32(np, "framerate", &params->framerate);
 
 	ret = of_property_read_string(np, "dma", &str);
 	if (ret) {
@@ -292,6 +296,8 @@ static int ddmafb_probe(struct platform_device *pdev) {
 		ret = -ENOMEM;
 		goto error_release_dma_channel;
 	}
+	// for some reason using the dma_cpu_addr doesn't work, idfk why
+	// this is kernel space so just gonna use the physical address
 
 	info = framebuffer_alloc(sizeof(struct ddmafb_par), &pdev->dev);
 	if (!info) {
@@ -303,7 +309,8 @@ static int ddmafb_probe(struct platform_device *pdev) {
 	par = info->par;
 
 	info->fix = ddmafb_fix;
-	info->fix.smem_start = (size_t) dma_cpu_addr;
+	//info->fix.smem_start = (size_t) dma_cpu_addr;
+	info->fix.smem_start = dma_handle;
 	info->fix.smem_len = dma_size;
 	info->fix.line_length = params.stride;
 
@@ -324,13 +331,25 @@ static int ddmafb_probe(struct platform_device *pdev) {
 	par->dma_handle = dma_handle;
 	par->dma_size = dma_size;
 
+	if (params.framerate > 0) {
+		par->frametime_us = 1000000 / params.framerate;
+		dev_info(&pdev->dev, "set to %d fps (%uuS/frame)!\n", params.framerate, par->frametime_us);
+	} else {
+		par->frametime_us = 0;
+		dev_info(&pdev->dev, "framerate limit not set!\n");
+	}
+
 	info->fbops = &ddmafb_ops;
-	info->screen_base = ioremap_wc(info->fix.smem_start, info->fix.smem_len);
-	if (!info->screen_base) {
+	info->pseudo_palette = par->palette;
+	info->screen_base = (char*) dma_handle;
+
+	//info->screen_base = (char*) dma_cpu_addr;
+	//info->screen_base = ioremap_wc(info->fix.smem_start, info->fix.smem_len);
+	/*if (!info->screen_base) {
+		dev_info(&pdev->dev, "failed to ioremap screen_base!\n");
 		ret = -ENOMEM;
 		goto error_fb_release;
-	}
-	info->pseudo_palette = par->palette;
+	}*/
 
 	dev_info(&pdev->dev, "framebuffer at 0x%lx, 0x%x bytes\n",
 			     info->fix.smem_start, info->fix.smem_len);
@@ -339,7 +358,8 @@ static int ddmafb_probe(struct platform_device *pdev) {
 			     info->var.xres, info->var.yres,
 			     info->var.bits_per_pixel, info->fix.line_length);
 
-	ret = devm_aperture_acquire_for_platform_device(pdev, (resource_size_t) par->dma_cpu_addr, par->dma_size);
+	//ret = devm_aperture_acquire_for_platform_device(pdev, (resource_size_t) par->dma_cpu_addr, par->dma_size);
+	ret = devm_aperture_acquire_for_platform_device(pdev, (resource_size_t) par->dma_handle, par->dma_size);
 	if (ret) {
 		dev_err(&pdev->dev, "Unable to acquire aperture: %d\n", ret);
 		goto error_unmap;
@@ -357,13 +377,13 @@ static int ddmafb_probe(struct platform_device *pdev) {
 		dev_err(&pdev->dev, "Failed to start DMA thread: %ld\n", PTR_ERR(dma_thread));
 		return PTR_ERR(dma_thread);
 	}
-	dev_info(&pdev->dev, "fb%d: started update thread!\n");
+	dev_info(&pdev->dev, "fb%d: started update thread!\n", info->node);
 
 	return 0;
 
 error_unmap:
-	iounmap(info->screen_base);
-error_fb_release:
+//	iounmap(info->screen_base);
+//error_fb_release:
 	framebuffer_release(info);
 error_release_dma_region:
 	dma_free_coherent(&pdev->dev, dma_size, dma_cpu_addr, dma_handle);
